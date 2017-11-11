f55ffd8d259551a202b18ec4190b0444e6186e43
John Duncan <john@johnduncanphoto.com>
2017-11-11 14:50:37 +0000

    tiger return value tested, tests passed

---
+++ app/src/test/java/com/example/johnduncan/weekendhomeworkzoo/TigerTest.java
@@ -0,0 +1,23 @@
+package com.example.johnduncan.weekendhomeworkzoo;
+
+import com.example.johnduncan.weekendhomeworkzoo.Animal.Tiger;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by John Duncan on 11/11/2017.
+ */
+
+public class TigerTest {
+
+    @Test
+    public void testTigerCanReturnValue(){
+        Tiger tiger = new Tiger();
+        tiger.setValue(1000);
+        int actual = tiger.getValue();
+        assertEquals(1000,actual);
+
+    }
+}
