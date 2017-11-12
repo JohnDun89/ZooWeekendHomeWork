733e61896680988c18474690e084c1c820d0aff1
John Duncan <john@johnduncanphoto.com>
2017-11-12 12:45:08 +0000

    visitor class with constructor created, tests written

---
+++ app/src/main/java/com/example/johnduncan/weekendhomeworkzoo/Visitor.java
@@ -0,0 +1,18 @@
+package com.example.johnduncan.weekendhomeworkzoo;
+
+/**
+ * Created by John Duncan on 12/11/2017.
+ */
+
+public class Visitor {
+
+     String name;
+     String postcode;
+     Integer funds;
+
+    public Visitor() {
+        this.name = name;
+        this.postcode = postcode;
+        this.funds = funds;
+    }
+}
---
+++ app/src/test/java/com/example/johnduncan/weekendhomeworkzoo/VisitorTest.java
@@ -0,0 +1,8 @@
+package com.example.johnduncan.weekendhomeworkzoo;
+
+/**
+ * Created by John Duncan on 12/11/2017.
+ */
+
+public class VisitorTest {
+}
